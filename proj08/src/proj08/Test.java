458da8eb1571a92523b1e7a3601b0be6be0fc746
kuei <ee80@aaa.jjj>
2015-06-24 13:33:06 +0800

    Add	System.out.println("version1");

--- proj08/src/proj08/Test.java
+++ proj08/src/proj08/Test.java
@@ -4,6 +4,12 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("version1");
+		System.out.println("version2");
+		System.out.println("version3");
+		System.out.println("version4");
+		System.out.println("version5");
+		System.out.println("version6");
+		System.out.println("version7");
 
 	}
 
